659dad4f5b05bb3a142cef36a863dbc8fb13e020
alderjunio <alder.junio@gmail.com>
2025-09-09 10:35:48 -0400

    implementando o main prompt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import prompt from "prompt" 
+import prompt from "prompt"
+import mainPrompt from "./prompts/prompt-main.js"
 
 async function main() {
-    
+    prompt.get(mainPrompt, async (err, choose) => {
+        if(choose.select == 1) console.log("escolheu o qrcode")   
+        if(choose.select == 2) console.log("escolheu o passqord")   
+    })
+
+    prompt.start()
 }
 
 main()
