e8cf847c2270d41d3180f9713ab43e7839bdf0b9
alderjunio <alder.junio@gmail.com>
2025-09-09 10:45:26 -0400

    utilizando o chalk nas msg do projeto

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import mainPrompt from "./prompts/prompt-main.js"
 async function main() {
     prompt.get(mainPrompt, async (err, choose) => {
         if(choose.select == 1) console.log("escolheu o qrcode")   
-        if(choose.select == 2) console.log("escolheu o passqord")   
+        if(choose.select == 2) console.log("escolheu o password")   
     })
 
     prompt.start()
