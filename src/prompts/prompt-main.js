465bf2e151342eea65c76c955fe56856195adc7a
alderjunio <alder.junio@gmail.com>
2025-09-09 10:56:41 -0400

    modificando estilo da msg com chalk

--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -3,9 +3,9 @@ import chalk from "chalk"
 const mainPrompt = [
     {
         name: "select",
-        description: chalk.yellow("Escolha a ferramenta (1 - QRCODE ou 2 - PASSWORD)"),
+        description: chalk.yellow.bold("Escolha a ferramenta (1 - QRCODE ou 2 - PASSWORD)"),
         pattern: /^[1-2]+$/,
-        message: chalk.red("Escolha apenas entre 1 e 2"),
+        message: chalk.red.italic.bold("Escolha apenas entre 1 e 2"),
         required: true,
     },
 ]
